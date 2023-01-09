c505db981997a14c332e5eedba9619f75daa3275
sethiyasanskar63 <101315790+sethiyasanskar63@users.noreply.github.com>
2023-01-09 23:57:47 +0530

    Delete G2AB4_BED_GradedProject3 directory

--- G2AB4_BED_GradedProject3/src/main/java/com/greatlearning/G2AB4_BED_GradedProject3/service/TicketCreateService.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.G2AB4_BED_GradedProject3.service;
-
-import com.greatlearning.G2AB4_BED_GradedProject3.entity.Ticket;
-
-public interface TicketCreateService {
-
-	void addSingleTicket(Ticket ticket);
-
-}
--- G2AB4_BED_GradedProject3/src/main/java/com/greatlearning/G2AB4_BED_GradedProject3/service/TicketDeleteService.java
+++
@@ -1,7 +0,0 @@
-package com.greatlearning.G2AB4_BED_GradedProject3.service;
-
-public interface TicketDeleteService {
-
-	void deleteTicketById(int id);
-
-}
